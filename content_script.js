3e856b9b42f90f5dfcde2ce3e6eca106c2779cdf
zhangzhoufeng@aichimedes.com <zhangzhoufeng@aichimedes.com>
2024-07-13 10:18:16 +0800

    feat: add style sheet

--- content_script.js
+++ content_script.js
@@ -1,13 +1,17 @@
 const LOGO = 'https://img.icons8.com/?size=48&id=59023&format=png'
 
 document.addEventListener('mouseup', function(event) {
+    var existingButton = document.querySelector('#chrome-ai-button');
+    if (existingButton) {
+        existingButton.remove();
+    }
     if (window.getSelection().toString().length > 0) {
         var button = document.createElement('button');
+        button.id = 'chrome-ai-button'
         button.innerHTML = `<img src="${LOGO}" alt="Icon" />`;
         button.style.position = 'fixed';
         button.style.top = event.clientY + 'px';
         button.style.left = event.clientX + 'px';
-        button.style.zIndex = 9999;
         document.body.appendChild(button);
     }
 });
