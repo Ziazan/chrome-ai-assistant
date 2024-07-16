0c4bb112632e212e24486bf24329ae650425975f
zhangzhoufeng@aichimedes.com <zhangzhoufeng@aichimedes.com>
2024-07-16 22:21:29 +0800

    style: use vue

--- src/content/components/app.vue
+++ src/content/components/app.vue
@@ -1,19 +1,57 @@
 <!--
  * @Date: 2024-07-15 21:11:32
- * @LastEditTime: 2024-07-15 22:24:08
+ * @LastEditTime: 2024-07-16 22:04:53
  * @Description: 请填写简介
 -->
 <template>
-    <h1 class="blue">CONTENT</h1>
+    <div class="">
+      <h1 class="blue">CONTENT123</h1>
+      <!-- ai提问按钮 -->
+      <button  v-show="isButtonShow" id="chrome-ai-button" :style="buttonStyle" class="sectionBtn" @click="handleClick">
+        <img :id="BTN_IMG_ID" :src="LOGO" alt="Icon" />
+      </button>
+    </div>
   </template>
   
-  <script lang="ts">
+  <script lang="ts" setup>
+  import {ref,onMounted,onUnmounted} from 'vue';
+  const LOGO = 'https://img.icons8.com/?size=48&id=59023&format=png'
+  const BTN_IMG_ID = 'ai-btn-img';
 
-  import { defineComponent } from 'vue';
+  const buttonStyle = ref({})
+  const isButtonShow = ref(false);
+  const sectionValue = ref('');
   
-  export default defineComponent({
-    name: 'App',
-  });
+  const handleClick = ()=>{
+    isButtonShow.value = false;
+    console.log('%c [ sectionValue ]-27', 'font-size:13px; background:pink; color:#bf2c9f;', sectionValue)
+  }
+  
+  onMounted(()=>{
+    document.addEventListener('mouseup',handleMouseUp);
+  })
+
+  const handleMouseUp = (event:MouseEvent)=>{
+    const userSelection = window.getSelection()?.toString() || '';
+    if (userSelection.length > 0) {
+      sectionValue.value = userSelection;
+      console.log('%c [ sectionValue.value ]-34', 'font-size:13px; background:pink; color:#bf2c9f;', sectionValue.value)
+      isButtonShow.value = true;
+      buttonStyle.value = {
+        top:event.clientY + 'px',
+        left:event.clientX + 'px',
+      }
+    }
+  }
+
+  onUnmounted(() => {
+    document.removeEventListener('mouseup',handleMouseUp);
+  })
+
+
+
+  
+  
   </script>
   
   <style lang="css" src="./app.css">
--- src/content/main.ts
+++ src/content/main.ts
@@ -23,31 +23,3 @@ function joinContent (element: any) {
 
   injectJsInsert ();
   joinContent(App)
-
-
-  const LOGO = 'https://img.icons8.com/?size=48&id=59023&format=png'
-const BTN_IMG_ID = 'ai-btn-img';
-
-document.addEventListener('mouseup', function(event) {
-    const existingButton = document.querySelector('#chrome-ai-button');
-    if (existingButton) {
-        existingButton.remove();
-    }
-    const isAiBtnClick = event.target ===  document.querySelector(`#${BTN_IMG_ID}`);
-    const userSelection = window.getSelection()?.toString() || '';
-    if(isAiBtnClick){
-        //TODO 请求接口
-        console.log('%c [ userSelection ]-11', 'font-size:13px; background:pink; color:#bf2c9f;', userSelection)
-
-    }else{
-        if (userSelection.length > 0) {
-            const button = document.createElement('button');
-            button.id = 'chrome-ai-button'
-            button.innerHTML = `<img id="${BTN_IMG_ID}" src="${LOGO}" alt="Icon" />`;
-            button.style.position = 'fixed';
-            button.style.top = event.clientY + 'px';
-            button.style.left = event.clientX + 'px';
-            document.body.appendChild(button);
-        }
-    }
-});
